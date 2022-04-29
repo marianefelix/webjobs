4a4c6e13ec2c59249b1cd5360abf08e219d9ce06
lorenatoscano <lorenatoscano@ufrn.edu.br>
2022-04-29 15:46:04 -0300

    feat: Adiciona componente FilterChip

---
+++ src/components/FilterChip/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import { FilterChipContainer, CloseButton, CloseIcon, Text } from './styled';
+
+interface FilterChipProps {
+  children: ReactNode;
+}
+
+export const FilterChip = ({ children, ...props }: FilterChipProps) => {
+  return (
+    <FilterChipContainer {...props}>
+      <Text>{children}</Text>
+      <CloseButton title="Remover filtro">
+        <CloseIcon aria-label="Remover filtro" />
+      </CloseButton>
+    </FilterChipContainer>
+  );
+};
