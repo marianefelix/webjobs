0ec954402a0ae5208db79d9d85b776db11956475
Mariane Felix <marianefelix59@gmail.com>
2022-06-03 19:48:41 -0300

    feat: altera local de variáveis de breakpoints

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -35,9 +35,9 @@ declare module 'styled-components' {
       filterBox: string;
       shareButton: string;
     };
-    breakpoints: {
-      sm: string;
-      lg: string;
+    device: {
+      mobile: string;
+      desktop: string;
     };
   }
 }
