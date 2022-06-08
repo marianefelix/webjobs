100f440f778501a34fbedacb9cf6c3dc0f6e959f
Mariane Felix <marianefelix59@gmail.com>
2022-06-08 09:41:30 -0300

    feat: altera estilo da página de cadastro de usuário

--- src/pages/UserRegistration/styles.ts
+++ src/pages/UserRegistration/styles.ts
@@ -1,10 +1,3 @@
-// header - logo
-// main
-// 1. two sections
-// - image -> position absolute (?)
-// - registration
-// footer
-
 import styled from 'styled-components';
 
 export const UserRegistrationContainer = styled.div`
@@ -26,17 +19,18 @@ export const Header = styled.header`
 
 export const Main = styled.main`
   display: flex;
-  align-items: center;
+  justify-content: space-between;
   flex: 1;
 
-  margin-top: 3.5rem;
+  gap: 1.5rem;
 `;
 
 export const ImageSection = styled.section`
   display: flex;
+  align-items: flex-end;
 
   svg {
-    width: 95%;
+    width: 90%;
     height: 70%;
   }
 
@@ -45,7 +39,13 @@ export const ImageSection = styled.section`
   }
 `;
 
-export const RegistrationSection = styled.section``;
+export const RegistrationSection = styled.section`
+  width: 70%;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
 
 export const Description = styled.h3`
   font-size: ${({ theme }) => theme.font.size.large};
@@ -55,6 +55,8 @@ export const Description = styled.h3`
 `;
 
 export const Form = styled.form`
+  width: 100%;
+
   & > div:nth-child(1) {
     margin-bottom: 2rem;
   }
