bbc27a116b22a3918f8a3833288fa8af5d9df91a
Mariane Felix <marianefelix59@gmail.com>
2022-06-23 21:47:49 -0300

    feat: adiciona botão de adicionar vaga de trabalho #46

--- src/pages/JobList/index.tsx
+++ src/pages/JobList/index.tsx
@@ -3,8 +3,13 @@ import { FilterBox } from 'components/FilterBox';
 import jobs from '../../data.json';
 import { Header } from 'components/Header';
 import { AbsoluteBox, Main } from './styles';
+import { AddJobButton } from 'components/Button/AddJob';
+
+import { useNavigate } from 'react-router-dom';
 
 export const JobList = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <Header />
@@ -15,6 +20,7 @@ export const JobList = () => {
         {jobs.map((job) => (
           <Card job={job} />
         ))}
+        <AddJobButton onClick={() => navigate('/new-job')} />
       </Main>
     </>
   );
