580bdc4582c43f87ae6dc8994d4766a2c8e71205
Mariane Felix <marianefelix59@gmail.com>
2022-06-26 16:55:15 -0300

    feat: cria service que query client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,16 +3,20 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { light } from './styles/theme';
 import { routes } from 'routes';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from 'services/queryClient';
 
 export const App = () => {
   return (
     <ThemeProvider theme={light}>
-      <GlobalStyle />
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route path={path} element={element} key={path} />
-        ))}
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Routes>
+          {routes.map(({ path, element }) => (
+            <Route path={path} element={element} key={path} />
+          ))}
+        </Routes>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
