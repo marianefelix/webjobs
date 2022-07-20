992d6606322deee99b163876950d878e5982d59a
Mariane Felix <marianefelix59@gmail.com>
2022-07-20 09:40:16 -0300

    add procfile e endereço de cors

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors('localhost:3000'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
