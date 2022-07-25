1a98d1b728ec51b63c733c3272fc6a99279a0298
Mariane Felix <marianefelix59@gmail.com>
2022-07-25 09:22:44 -0300

    feat: altera endereço de api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://154.53.36.47',
+  baseURL: 'http://localhost:3333',
   headers: {
     'Content-Type': 'application/json',
   },
